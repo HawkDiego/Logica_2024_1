d92c621e1069489b301b297b608e0c4e1b95107b
HawkDiego <judgiraldogo@unal.edu.co>
2024-03-08 20:56:39 -0500

    Punto 2 parcial

--- ejercicio/Program.cs
+++ ejercicio/Program.cs
@@ -4,36 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Ingresa tu salario base: ");
-        double salary = Convert.ToDouble(Console.ReadLine());
+        double trainSpeed1 = 80;
+        double trainSpeed2 = 100;
 
-        Console.Write("Valor venta 1: ");
-        double sell1 = Convert.ToDouble(Console.ReadLine());
-        double commission1 = sell1 * 0.1;
-        Console.Write("Valor venta 2: ");
-        double sell2 = Convert.ToDouble(Console.ReadLine());
-        double commission2 = sell2 * 0.1;
-        Console.Write("Valor venta 3: ");
-        double sell3 = Convert.ToDouble(Console.ReadLine());
-        double commission3 = sell3 * 0.1;
+        double initialTime = 1;
 
-        double totalCommission = commission1 + commission2 + commission3;
+        double distanceBetweenTrains = trainSpeed1* initialTime;
 
-        double totalSells = sell1 + sell2 + sell3;
+        double timeToCatchUp = distanceBetweenTrains / (trainSpeed2 - trainSpeed1);
 
-        double salaryMoreCommissions = totalCommission + salary;
 
-        double maxSell = Math.Max(sell1, Math.Max(sell2, sell3));
+        double catchUpTime = timeToCatchUp + 10;
+   
 
-        double averageCommission = totalCommission / 3;
 
-        Console.WriteLine($"Dinero por concepto de comisiones: {totalCommission}");
-        Console.WriteLine($"Comisiones mas salario base: {salaryMoreCommissions}");
-        Console.WriteLine($"Venta que generó mas comision: {maxSell}");
-        Console.WriteLine($"Promedio de comisiones: {averageCommission}");
+        Console.WriteLine($"El segundo tren alcanzará al primer tren a las {catchUpTime} AM.");
 
-        if (totalSells >= 1000000) Console.WriteLine("Ganaste el beneficio");
-        else Console.WriteLine("No ganaste el beneficio");
 
     }
 }
